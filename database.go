072f278442f1e584682f22332a81e71fca06e086
DavidHODs <davidoluwatobi41@gmail.com>
2023-03-09 07:20:59 +0100

    set up env variables

--- database.go
+++ database.go
@@ -3,11 +3,28 @@ package main
 import (
 	"database/sql"
 	"fmt"
+	"log"
+	"os"
+	"strconv"
 
+	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
 )
 
+func loadEnv() (string, int, string, string, string) {
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatal("could not load environment file")
+	}
+
+	port, _ := strconv.Atoi(os.Getenv("port"))
+
+	return os.Getenv("host"), port, os.Getenv("user"), os.Getenv("password"), os.Getenv("dbname")
+}
+
 func database() {
+	host, port, user, password, dbname := loadEnv()
+
 	info := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
 
 	db, err := sql.Open("postgres", info)
