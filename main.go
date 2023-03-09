6c4919fa0d95bb8a1441ea71eaf37fe5ead58772
DavidHODs <davidoluwatobi41@gmail.com>
2023-03-09 11:40:46 +0100

    post form linked to database

--- main.go
+++ main.go
@@ -20,39 +20,50 @@ type dataStruct struct {
 
 // GET renders the html page, POST inserts form record into the database
 func renderPage(res http.ResponseWriter, req *http.Request) {
-	if req.Method == "GET" {
-		page, err := template.ParseFiles("index.tmpl")
-		if err != nil {
-			http.Error(res, err.Error(), http.StatusInternalServerError)
-		}
-
-		rows, err := db.Query(`SELECT name, comment FROM record;`)
-		if err != nil {
-			fmt.Println(err)
-		}
-
-		defer rows.Close()
-
-		data := dataStruct{}
-		results := []dataStruct{}
-
-		for rows.Next() {
-			var name string
-			var comment string
-			if err := rows.Scan(&name, &comment); err != nil {
-				http.Error(res, err.Error(), http.StatusInternalServerError)
-			}
-			data.Name = name
-			data.Comment = comment
-			results = append(results, data)
-		}
-
-		// renders and passes data to the template file
-		err = page.Execute(res, results)
-		if err != nil {
-			http.Error(res, err.Error(), http.StatusInternalServerError)
-		}
+	page, err := template.ParseFiles("index.tmpl")
+	if err != nil {
+		http.Error(res, err.Error(), http.StatusInternalServerError)
 	}
+
+	rows, err := db.Query(`SELECT name, comment FROM record ORDER BY created_at DESC;`)
+	if err != nil {
+		http.Error(res, err.Error(), http.StatusInternalServerError)
+	}
+
+	defer rows.Close()
+
+	data := dataStruct{}
+	results := []dataStruct{}
+
+	for rows.Next() {
+		var name string
+		var comment string
+		if err := rows.Scan(&name, &comment); err != nil {
+			http.Error(res, err.Error(), http.StatusInternalServerError)
+		}
+		data.Name = name
+		data.Comment = comment
+		results = append(results, data)
+	}
+
+	// renders and passes data to the template file
+	err = page.Execute(res, results)
+	if err != nil {
+		http.Error(res, err.Error(), http.StatusInternalServerError)
+	}
+}
+
+func insertRecord(res http.ResponseWriter, req *http.Request) {
+	req.ParseForm()
+	name := req.Form.Get("name")
+	message := req.Form.Get("message")
+
+	stmt := `insert into record(name, comment) values($1, $2)`
+	_, err := db.Exec(stmt, name, message)
+	if err != nil {
+		http.Error(res, err.Error(), http.StatusInternalServerError)
+	}
+	http.Redirect(res, req, "/", http.StatusSeeOther)
 }
 
 func main() {
@@ -61,6 +72,7 @@ func main() {
 	router := mux.NewRouter()
 
 	router.HandleFunc("/", renderPage).Methods("GET")
+	router.HandleFunc("/insertrecord", insertRecord).Methods("POST")
 	// loads static files to be rendered
 	router.PathPrefix("/static/").Handler(http.StripPrefix("/static/", http.FileServer(http.Dir("./static"))))
 
